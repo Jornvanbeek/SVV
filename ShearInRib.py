22566560fc207a03aa879f61d8a2f27159d59fe3
ThomasAeroDelft <44244338+ThomasAeroDelft@users.noreply.github.com>
2019-02-25 14:22:55 +0100

    Add files via upload

--- ShearInRib.py
+++ ShearInRib.py
@@ -40,18 +40,18 @@ C=1.611*10**3
 h=0.161*10**3
 theta = m.radians(25.)
 P  = -20600.0 
-A=np.matrix([[ 20923.17083281],
-        [  6219.1927298 ],
-        [     0.        ],
-        [-31740.2740972 ],
-        [ -1131.61368329],
-        [ 10103.24385237],
-        [  -251.0070356 ],
-        [-24681.73285221],
-        [-10517.69545886],
-        [  8680.4080557 ],
-        [  -756.92066185],
-        [   131.72470617]])
+A=np.matrix([[ 6.56326488e+04],
+ [-2.99280085e+04],
+ [ 0.00000000e+00],
+ [-9.32054504e+04],
+ [ 2.73538743e+04],
+ [ 3.23659761e+04],
+ [-1.56076390e+03],
+ [ 5.03843396e+04],
+ [-4.55168632e+03],
+ [ 1.70401690e+03],
+ [ 6.39377657e+02],
+ [-7.99398950e+01]])
 n=1000
 #xpos = [int(78/1000*n),int(233/1000*n),int(309/1000*n),int(385/1000*n),int(927/1000*n)]
 ribflow=[]
@@ -60,10 +60,23 @@ Ry=A[0]
 Rz=A[1]
 Rib = RibShearHinge(Ry, Rz, C, h)
 ribflow.append(Rib)
+#at F
+Rz = A[7] * m.cos(theta)
+Ry = A[7] * m.sin(theta)
+Rib = RibShearActuator(Ry,Rz,C,h)
+ribflow.append(Rib)
+
 #hinge 2
 Ry=A[3]
 Rz=A[4]
 Rib = RibShearHinge(Ry, Rz, C, h)
+ribflow.append(Rib)
+
+
+#at P 
+Rz = P * m.cos(theta)
+Ry = P * m.sin(theta)
+Rib = RibShearActuator(Ry,Rz,C,h)
 ribflow.append(Rib)
 #hinge 3
 Ry=A[5]
@@ -71,16 +84,4 @@ Rz=A[6]
 Rib = RibShearHinge(Ry, Rz, C, h)
 ribflow.append(Rib)
 
-#at P 
-Rz = -P * m.cos(theta)
-Ry = -P * m.sin(theta)
-Rib = RibShearActuator(Ry,Rz,C,h)
-ribflow.append(Rib)
-#at F
-Rz = A[7] * m.cos(theta)
-Ry = A[7] * m.sin(theta)
-Rib = RibShearActuator(Ry,Rz,C,h)
-ribflow.append(Rib)
 print(ribflow)
-
-
